8360aa5c2b7dd45deb47a2acfb6e0277ff50a9e1
Ubuntu <ubuntu@ip-172-31-63-22.ec2.internal>
2016-10-27 16:13:27 +0000

    Add proc time to R code

--- pipeline/test/Submit_RNAseq.DLBC.sh
+++ pipeline/test/Submit_RNAseq.DLBC.sh
@@ -10,4 +10,4 @@ now=$(date +"%m-%d-%Y_%H:%M:%S")
 
 ## submit job 
 
-bds -c /home/ubuntu/dev/rnaseq/CRI-Workshop-Nov2016-RNAseq/test/myProject/DLBC.bds.cfg  -y 0    ../Run_RNAseq.bds -aligners star -callers featurecounts -projdir /home/ubuntu/dev/rnaseq/CRI-Workshop-Nov2016-RNAseq/test/myProject -project DLBC -samples KO01 WT01 > Run_RNAseq.DLBC.$now.log.out 2> Run_RNAseq.DLBC.$now.log.err
+bds -c /home/ubuntu/dev/rnaseq/CRI-Workshop-Nov2016-RNAseq/pipeline/test/myProject/DLBC.bds.cfg  -y 0    ../Run_RNAseq.bds -aligners star -callers featurecounts -projdir /home/ubuntu/dev/rnaseq/CRI-Workshop-Nov2016-RNAseq/pipeline/test/myProject -project DLBC -samples KO01 WT01 > Run_RNAseq.DLBC.$now.log.out 2> Run_RNAseq.DLBC.$now.log.err
